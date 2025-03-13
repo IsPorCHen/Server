8df65b3c05cadd2c273428d6c861ff62302ac427
IsPorCHen <fazy2019@gmail.com>
2025-03-14 00:58:21 +0500

    стабильная версия

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -6,8 +6,8 @@
 #include <Windows.h>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
-#include <iphlpapi.h>
 #include <string>
+#include <stdio.h>
 
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -47,7 +47,6 @@ DWORD WINAPI ReceiveMessages(LPVOID lpParam) {
     }
     return 0;
 }
-
 
 int main(int argc, char* argv[]) {
     setlocale(0, "rus");
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,7 +1,8 @@
-﻿#define WIN32_LEAN_AND_MEAN
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN
 #define PORT "8080"
 #define BUFLEN 512
+#define SPAM_TIMEOUT 5
 
 #include <iostream>
 #include <Windows.h>
@@ -11,6 +12,7 @@
 #include <vector>
 #include <string>
 #include <ctime>
+#include <stdio.h>
 
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -22,25 +24,45 @@ int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 struct addrinfo* result = NULL, * ptr = NULL, hints;
 
 std::map<int, std::string> clients;
+std::map<int, time_t> lastMessageTime;  // Временные метки последнего сообщения для каждого клиента
 std::vector<std::string> chatHistory;
 HANDLE hMutex;
 
-std::string getCurrentTime() {
-    time_t now = time(0);
-    char buf[80];
-    struct tm* timeinfo = localtime(&now);
-    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", timeinfo);
-    return std::string(buf);
+void logToFile(const std::string& message) {
+    FILE* logFile = fopen("chat_log.txt", "a");
+    if (logFile != NULL) {
+        fprintf(logFile, "%s\n", message.c_str());
+        fclose(logFile);
+    }
+    else {
+        printf("Не удалось открыть файл для записи!\n");
+    }
 }
 
 void sendToAllClients(const std::string& message, int senderSocket = -1) {
     WaitForSingleObject(hMutex, INFINITE);
+
+    logToFile(message);
+
     for (const auto& client : clients) {
         if (client.first != senderSocket) {
             send(client.first, message.c_str(), message.size(), 0);
         }
     }
+
     ReleaseMutex(hMutex);
+}
+
+bool isSpam(int clientSocket) {
+    time_t currentTime = time(0);
+    if (lastMessageTime.find(clientSocket) != lastMessageTime.end()) {
+        double timeDifference = difftime(currentTime, lastMessageTime[clientSocket]);
+        if (timeDifference < SPAM_TIMEOUT) {
+            return true; // Спам: отправка сообщений слишком быстро
+        }
+    }
+    lastMessageTime[clientSocket] = currentTime;  // Обновляем временную метку
+    return false;
 }
 
 DWORD WINAPI ClientThread(LPVOID lpParam) {
@@ -60,6 +82,7 @@ DWORD WINAPI ClientThread(LPVOID lpParam) {
 
     sendToAllClients(clientName + " присоединился в чат.");
 
+    // Отправляем историю чата новому пользователю
     for (const auto& msg : chatHistory) {
         send(clientSocket, msg.c_str(), msg.size(), 0);
     }
@@ -68,9 +91,39 @@ DWORD WINAPI ClientThread(LPVOID lpParam) {
         iResult = recv(clientSocket, recvbuf, BUFLEN, 0);
         if (iResult > 0) {
             std::string message(recvbuf, iResult);
-            char chatMessage[BUFLEN];
-            sprintf(chatMessage, "[%s]: %s", clientName.c_str(), message.c_str());
-            sendToAllClients(chatMessage, clientSocket);
+
+            // Проверка на спам
+            if (isSpam(clientSocket)) {
+                send(clientSocket, "Вы отправляете сообщения слишком быстро. Попробуйте позже.", 59, 0);
+                continue;
+            }
+
+            // Обработка команд
+            if (message[0] == '/') {
+                if (message == "/users") {
+                    std::string userList = "Активные пользователи: ";
+                    for (const auto& client : clients) {
+                        userList += client.second + ", ";
+                    }
+                    if (userList.length() > 0) {
+                        userList.pop_back();
+                        userList.pop_back();
+                    }
+                    send(clientSocket, userList.c_str(), userList.length(), 0);
+                }
+                else if (message == "/exit") {
+                    send(clientSocket, "Выход из чата...", 17, 0);
+                    break;
+                }
+                else {
+                    send(clientSocket, "Неизвестная команда.", 21, 0);
+                }
+            }
+            else {
+                char chatMessage[BUFLEN];
+                sprintf(chatMessage, "[%s]: %s", clientName.c_str(), message.c_str());
+                sendToAllClients(chatMessage, clientSocket);
+            }
         }
     } while (iResult > 0);
 
@@ -82,7 +135,6 @@ DWORD WINAPI ClientThread(LPVOID lpParam) {
     ReleaseMutex(hMutex);
     return 0;
 }
-
 
 int main(int argc, char* argv[]) {
     setlocale(0, "rus");
@@ -146,7 +198,6 @@ int main(int argc, char* argv[]) {
             printf("Ошибка accept: %d\n", WSAGetLastError());
             continue;
         }
-
 
         printf("[Сервер] Новый клиент подключился.\n");
 
